3c1d9c8a4e4a7234ea5ee5ddbd9b79a54837035b
deep3i <deepjpatel33@gmail.com>
2024-02-17 17:54:59 +0530

    ss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "@shopify/polaris/build/esm/styles.css";
 import { AppProvider } from "@shopify/polaris";
+import enTranslations from '@shopify/polaris/locales/en.json';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <AppProvider i18n={enTranslations}>
         <Component {...pageProps} />
+    </AppProvider>
     </>
   );
 }
